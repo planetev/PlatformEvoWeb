afcbabf42d03899c3963d70aff3d7e30e1c20f56
devaeem <tattapanforwork@gmail.com>
2024-09-11 16:27:17 +0700

    fff

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 // next.config.js
 module.exports = {
- images: {
-   remotePatterns: [
-     {
-      protocol: 'http',
-      hostname: 'localhost',
-      port: '3001', // ใส่พอร์ตที่เซิร์ฟเวอร์รูปภาพใช้อยู่
-      pathname: '/upload/images/**',
-     },
-   ],
- },
-}
+  basePath: "/evp",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3001", // ใส่พอร์ตที่เซิร์ฟเวอร์รูปภาพใช้อยู่
+        pathname: "/upload/images/**",
+      },
+    ],
+  },
+};

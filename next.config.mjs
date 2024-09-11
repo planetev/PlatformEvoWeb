1881f23ed25c05f9fcc09c3547e10aa5e0c80370
Phichaiyutp <phichaiyut.psb@gmail.com>
2024-09-11 17:15:23 +0700

    edit next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/evp",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3001", // ใส่พอร์ตที่เซิร์ฟเวอร์รูปภาพใช้อยู่
+        pathname: "/upload/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
